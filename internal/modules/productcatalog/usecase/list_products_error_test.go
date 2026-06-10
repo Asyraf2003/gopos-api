0c636ffb8431ba7debfc0f8b170ac6ec73c9af90
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-06-10 18:28:31 +0800

    commit 188

---
+++ internal/modules/productcatalog/usecase/list_products_error_test.go
@@ -0,0 +1,48 @@
+package usecase
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"pos-go/internal/modules/productcatalog/domain"
+	"pos-go/internal/modules/productcatalog/ports"
+)
+
+func TestListProductsPropagatesReaderListError(t *testing.T) {
+	listErr := errors.New("list products failed")
+	usecase := NewListProducts(&listProductsReaderDouble{
+		listErr: listErr,
+	})
+
+	_, err := usecase.Execute(context.Background(), ListProductsQuery{})
+
+	if !errors.Is(err, listErr) {
+		t.Fatalf("Execute() error = %v, want %v", err, listErr)
+	}
+}
+
+type listProductsReaderDouble struct {
+	listErr error
+}
+
+func (d *listProductsReaderDouble) GetByID(
+	_ context.Context,
+	_ string,
+) (*domain.Product, error) {
+	return nil, nil
+}
+
+func (d *listProductsReaderDouble) List(
+	_ context.Context,
+	_ ports.ProductListQuery,
+) ([]ports.ProductListItem, error) {
+	return nil, d.listErr
+}
+
+func (d *listProductsReaderDouble) Lookup(
+	_ context.Context,
+	_ ports.ProductLookupQuery,
+) ([]ports.ProductLookupItem, error) {
+	return nil, nil
+}
