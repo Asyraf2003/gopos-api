d8d80316a048d7e832b2f0cd6043893d1220d05b
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-06-10 13:35:39 +0800

    commit 174

---
+++ internal/modules/productcatalog/usecase/restore_product_version_error_test.go
@@ -0,0 +1,48 @@
+package usecase
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	"pos-go/internal/modules/productcatalog/domain"
+)
+
+func TestRestoreProductPropagatesVersionAppendError(t *testing.T) {
+	product, err := domain.NewProduct(domain.ProductInput{
+		ID:              "product-1",
+		Name:            "Filter Udara",
+		Brand:           "Aspira",
+		SalePriceRupiah: 30000,
+	})
+	if err != nil {
+		t.Fatalf("NewProduct() error = %v", err)
+	}
+
+	if err := product.SoftDelete(domain.DeleteInput{
+		DeletedAt:        time.Date(2026, 6, 10, 9, 0, 0, 0, time.UTC),
+		DeletedByActorID: "actor-1",
+		Reason:           "setup deleted product",
+	}); err != nil {
+		t.Fatalf("SoftDelete() setup error = %v", err)
+	}
+
+	versionErr := errors.New("version append failed")
+	usecase := NewRestoreProduct(
+		&restoreProductRepositoryDouble{found: product},
+		&fakeProductVersionRepository{err: versionErr},
+		&fakeProductAuditRecorder{},
+		func() time.Time { return time.Date(2026, 6, 10, 11, 0, 0, 0, time.UTC) },
+	)
+
+	_, err = usecase.Execute(context.Background(), RestoreProductCommand{
+		ID:      "product-1",
+		ActorID: "actor-2",
+		Reason:  "restore product",
+	})
+
+	if !errors.Is(err, versionErr) {
+		t.Fatalf("expected version append error, got %v", err)
+	}
+}
